cb7ea4298cc5eda3ce1a09974291710b1b28a0f8
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-10-05 12:21:32 +0100

    Embed style in img src={logo}.

--- React101/dash-101/src/App.js
+++ React101/dash-101/src/App.js
@@ -91,11 +91,12 @@ function MyButton2() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" 
+          style={{height: 150}}/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
+          <a 
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
