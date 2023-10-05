961ed6feeb463bc70cadf47403bad0671d9c2802
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-10-05 12:08:22 +0100

    Add image, via URL, to App.js (Profile function).

--- React101/dash-101/src/App.js
+++ React101/dash-101/src/App.js
@@ -2,6 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}: Output from Profile function.</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
 function ParseList() {
 
   const listsample = [
@@ -96,6 +119,7 @@ function MyButton2() {
         <MyButton2 />
         <AboutPage />
         <ParseList />
+        <Profile />
       </div>
     );
   }
