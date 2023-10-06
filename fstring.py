051bb1b8d27003b590870482a1167948cc8f7ef6
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-10-06 08:38:46 +0100

    More sophisticated f string and dictionary embedding.  Easy when you know how!!

--- fstring.py
+++ fstring.py
@@ -16,9 +16,12 @@ print(f"This is an example of a formatted float number: {f1:.02f}")
 print("text without f string", f1)
 print("keys: ", d1.keys())
 things = []
-things = d1.keys()
-print(f"Type of things: ", type(things))
-# thing = random.choice(things)
-print(type(d1.keys))
 
-print(f"rosesdesc is {d1[f'rosesdesc']}")
+for key in d1.keys():
+    things.append(key)
+
+print(f"Type of things: ", type(things), things)
+thing = random.choice(things)
+
+
+print(f"{thing} is {d1[thing]}")
---
+++ fstringdict.py
@@ -0,0 +1,12 @@
+import random
+d1 = {
+    "eleanor": {"eleanordesc": "child", "eleanorage": 15},
+    "roses": {"rosesdesc": "spotty cat", "rosesage": 10},
+    "maple": {"mapledesc": "mapley cat", "mapleage": "roses age + 3 minutes"},
+    "alaca": {"alacadesc": "shaggy dog", "alacaage": "senile"}
+}
+things = []
+for key in d1.keys():
+    things.append(key)
+thing = random.choice(things)
+print(f"{thing} is a {d1[thing][f'{thing}desc']}, aged: {d1[thing][f'{thing}age']}")
