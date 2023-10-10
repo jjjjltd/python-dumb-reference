34798f1763bfc3bf48571968566a531365b19cf0
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-10-10 09:39:23 +0100

    Parm pass to handler + setcount - both event outcomes visible.

--- React101/dash-101/src/sharedcounter.jsx
+++ React101/dash-101/src/sharedcounter.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function MyButton3() {
 
+  const [count_shared, setcount_shared] = useState(0);
   const handleClick3 = (num) => {
-    alert(num + ' abc')
+    setcount_shared(count_shared + 1)
+    alert(num + ' is the number passed in.  Press count = ' + count_shared)
     return () => {
       <p>Return value: {num}</p>
     }
@@ -12,7 +15,7 @@ export default function MyButton3() {
     const num = 3
     return (
       
-      <button onClick={() => handleClick3(num)}>Button</button>
+      <button onClick={() => handleClick3(num)}>{num} Button pressed {count_shared}</button>
       
     )
 
