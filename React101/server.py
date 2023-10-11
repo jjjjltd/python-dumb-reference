d20aca22ff836a068796e78356ed41ddb5048ebb
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-10-11 17:20:28 +0100

    Sci Fi app with front end v0.001.

--- React101/server.py
+++ React101/server.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
+model = []
 
 @app.route("/")
 def root():
@@ -24,6 +26,42 @@ def parms2(p1, p2):
         retstr = retstr + f"\nPrinting parameter2: {p2}"
 
     return retstr
+
+@app.get('/friend')
+def get_friends():
+    return model
+
+@app.get('/friend/<int:id>')
+def get_one_friend(id):
+    return model[id], 200
+
+@app.post('/friend')
+def create_friend():
+    request_data = request.get_json()
+    new_friend = {"name": request_data["name"], "id": len(model), "aob": "any other bollocks"}
+    model.append(new_friend)
+    return new_friend, 201
+
+@app.delete('/friend/<int:id>')
+def delete_friend(id):
+    del model[id]
+    return {"success": "data successfully deleted from the server"}, 200
+
+# @app.route("/body", methods=["POST"])
+# def body():
+
+#     data = request.json()
+#     print(data)
+#     name = request.form["name"]
+#     location = request.form["location"]
+
+#     ret_dict = {}
+#     ret_dict = {
+#         "name": name,
+#         "location": location
+#     }
+
+#     return f"The data you passed in was {ret_dict}"
 
 if __name__ == "__main__":
     app.run(debug=True)
